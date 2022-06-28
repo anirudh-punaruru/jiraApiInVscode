cf470b9d1fa98f59491c0207a2f830d7ed7f29d7
anirudh punaruru <anirudhpunaruru1999@gmail.com>
2022-06-28 12:54:51 +0530

    new changes

--- jiraApi.py
+++ jiraApi.py
@@ -13,13 +13,13 @@ query = {
    'jql': 'project = PS'
 }
 
-response_=requests.get(url, headers=headers, params=query, auth=('anirudhpunaruru1999@gmail.com', 'X79BUmVfIjaynmP0Vu7672CB'))
+response_=requests.get(url, headers=headers, params=query, auth=('anirudhpunaruru1999@gmail.com', 'ufrEHjE9u76HvTplXckS1857'))
 data= response_.json()
 issues= data['issues']
 for issue in issues:
    key= issue['key']
-   issue_url= 'https://sample-project-anirudh.atlassian.net/rest/api/3/issue'+key
-   res= requests.get(issue_url, headers=headers, auth=('anirudhpunaruru1999@gmail.com', 'X79BUmVfIjaynmP0Vu7672CB'))
+   issue_url= 'https://sample-project-anirudh.atlassian.net/rest/api/3/issue/'+key
+   res= requests.get(issue_url, headers=headers, auth=('anirudhpunaruru1999@gmail.com', 'ufrEHjE9u76HvTplXckS1857'))
    data= res.json()
    print(data['fields']['status']['name'])
 
