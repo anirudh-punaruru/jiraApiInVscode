36f474d15a7f2023a6c70a8946dfecfcd61a8a35
anirudh punaruru <anirudhpunaruru1999@gmail.com>
2022-06-28 12:46:26 +0530

    api

--- jiraApi.py
+++ jiraApi.py
@@ -17,5 +17,11 @@ response_=requests.get(url, headers=headers, params=query, auth=('anirudhpunarur
 data= response_.json()
 issues= data['issues']
 for issue in issues:
-    print(issue['key'])
+   key= issue['key']
+   issue_url= 'https://sample-project-anirudh.atlassian.net/rest/api/3/issue'+key
+   res= requests.get(issue_url, headers=headers, auth=('anirudhpunaruru1999@gmail.com', 'X79BUmVfIjaynmP0Vu7672CB'))
+   data= res.json()
+   print(data['fields']['status']['name'])
+
+
 
